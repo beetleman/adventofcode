fe8fd4414cbe8d8239dcb8d8745db8219a55f49d
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2023-12-01 23:47:25 +0100

    solve day 1 in common lisp

--- 2023/lisp/src/main.lisp
+++ 2023/lisp/src/main.lisp
@@ -1,5 +1,17 @@
 (defpackage beetleman.aoc-2023
-  (:use :cl))
+  (:use :cl)
+  (:export :solve))
 (in-package :beetleman.aoc-2023)
 
-;; blah blah blah.
+(defvar re-numbers (ppcre:create-scanner "\\d+"))
+
+(defun solve (calibration-document)
+  (reduce '+
+	  (loop for line in (ppcre:split "\\n" calibration-document)
+		collect (let ((numbers (ppcre:all-matches-as-strings re-numbers line)))
+			  (if numbers
+			      (parse-integer (format nil
+						     "~a~a"
+						     (first numbers)
+						     (car (last numbers))))
+			      0)))))
--- 2023/lisp/tests/main.lisp
+++ 2023/lisp/tests/main.lisp
@@ -6,6 +6,12 @@
 
 ;; NOTE: To run this test file, execute `(asdf:test-system :beetleman.aoc-2023)' in your Lisp.
 
+(defvar calibration-document
+  "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet")
+
 (deftest test-target-1
-  (testing "should (= 1 1) to be true"
-    (ok (= 1 1))))
+  (testing "calibration book should be solved"
+    (ok (= 142 (solve calibration-document)))))
