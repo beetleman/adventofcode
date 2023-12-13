d452e1198ef69f35266fa8b8398f8af46aa4cf79
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2023-12-13 19:04:52 +0100

    solve day 8 in CL (part 2)

--- 2023/lisp/src/day_8.lisp
+++ 2023/lisp/src/day_8.lisp
@@ -3,7 +3,8 @@
   (:import-from :ppcre)
   (:import-from :alexandria)
   (:import-from :serapeum)
-  (:export :solve))
+  (:export :solve-1
+	   :solve-2))
 (in-package :beetleman.aoc-2023.day-8)
 
 (defstruct node l r)
@@ -14,29 +15,44 @@
       (node-l node)
       (node-r node)))
 
-(defun count-steps (network)
+(defun count-steps (network start-node stop-re)
   (with-slots (nodes instructions) network
     (labels ((recur (current
 		     instructions
 		     counter)
-	       (if (string= "ZZZ" current)
-		   counter
+	       (if (ppcre:scan stop-re current)
+                   counter
 		   (recur (call-instruction (gethash current nodes)
 					    (first instructions))
-			  (alexandria:rotate instructions -1)
+			  (rest instructions)
 			  (+ counter 1)))))
-      (recur "AAA"
-	     instructions
+      (recur start-node
+	     (apply #'alexandria:circular-list instructions)
 	     0))))
 
 (defun parse (map-string)
   (let ((nodes (serapeum:dict))
 	(instructions (coerce (ppcre:scan-to-strings "^[LR]+" map-string) 'list)))
     (ppcre:do-register-groups (node l-node r-node)
-	("([A-Z]+) = \\(([A-Z]+), ([A-Z]+)\\)" map-string)
-      (setf (gethash node nodes) (make-node :l l-node :r r-node)))
+	("([A-Z0-9]+) = \\(([A-Z0-9]+), ([A-Z0-9]+)\\)" map-string)
+      (setf (gethash node nodes) (make-node :l l-node
+					    :r r-node)))
     (make-network :nodes nodes
 		  :instructions instructions)))
 
-(defun solve (network-string)
-  (count-steps (parse network-string)))
+(defun solve-1 (network-string)
+  (count-steps (parse network-string)
+	       "AAA"
+	       "ZZZ"))
+
+(defun solve-2 (network-string)
+  (let* ((network (parse network-string))
+	 (nodes (network-nodes network)))
+    (apply
+     #'lcm
+     (loop :for node :in (alexandria:hash-table-keys nodes)
+	   :when (char= (char node 2)
+			#\A)
+	     :collect (count-steps network
+				 node
+				 "..Z")))))
