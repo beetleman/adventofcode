894bbcff2a84a115656ef0c693d6459280953af0
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2023-12-21 20:23:06 +0100

    solve day 1 in CL (part 2)

--- 2023/lisp/src/day_1.lisp
+++ 2023/lisp/src/day_1.lisp
@@ -1,17 +1,41 @@
 (defpackage beetleman.aoc-2023.day-1
   (:use :cl)
-  (:export :solve-1))
+  (:export :solve-1
+	   :solve-2))
 (in-package :beetleman.aoc-2023.day-1)
 
-(defparameter re-numbers (ppcre:create-scanner "\\d"))
+(defparameter re-numbers (ppcre:create-scanner "(\\d)"))
+(defparameter re-numbers-en
+  (ppcre:create-scanner "(?=(zero|one|two|three|four|five|six|seven|eight|nine|\\d))"))
+
+(defparameter number-to-digit-mapping
+  (alexandria:alist-hash-table
+   (mapcar (lambda (n)
+	     (cons (format nil "~r" n) n))
+	   (alexandria:iota 10))
+   :test #'equal))
+
+(defun find-numbers (regex string transform)
+  (let ((numbers '()))
+    (ppcre:do-register-groups (n) (regex string)
+      (push (funcall transform n) numbers))
+    (reverse numbers)))
+
+(defun solve* (calibration-document regex transform)
+  (loop :for line :in (ppcre:split "\\n" calibration-document)
+	:for numbers = (find-numbers regex line transform)
+	:when numbers
+	  :summing (parse-integer (format nil
+					  "~a~a"
+					  (first numbers)
+					  (car (last numbers))))))
 
 (defun solve-1 (calibration-document)
-  (reduce '+
-	  (loop for line in (ppcre:split "\\n" calibration-document)
-		collect (let ((numbers (ppcre:all-matches-as-strings re-numbers line)))
-			  (if numbers
-			      (parse-integer (format nil
-						     "~a~a"
-						     (first numbers)
-						     (car (last numbers))))
-			      0)))))
+  (solve* calibration-document re-numbers #'identity))
+
+(defun solve-2 (calibration-document)
+  (solve* calibration-document
+	  re-numbers-en
+	  (lambda (n)
+	    (or (gethash n number-to-digit-mapping)
+		n))))
--- 2023/lisp/tests/day_1.lisp
+++ 2023/lisp/tests/day_1.lisp
@@ -7,14 +7,27 @@
 
 ;; NOTE: To run this test file, execute `(asdf:test-system :beetleman.aoc-2023)' in your Lisp.
 
-(defvar calibration-document
+(defvar calibration-document-1
   "1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet")
 
+(defvar calibration-document-2
+  "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen")
+
 (deftest solve-test
   (testing "Day 1: Trebuchet?!"
     (testing "part-1"
-      (ok (= 142 (solve calibration-document)))
-      (ok (= 55002 (solve (read-file "tests/day_1_input_part_1.txt")))))))
+      (ok (= 142 (solve-1 calibration-document-1)))
+      (ok (= 55002 (solve-1 (read-file "tests/day_1_input_part_1.txt")))))
+    (testing "part-2"
+      (ok (= 142 (solve-2 calibration-document-1)))
+      (ok (= 281 (solve-2 calibration-document-2)))
+      (ok (= 0 (solve-2 (read-file "tests/day_1_input_part_2.txt")))))))
