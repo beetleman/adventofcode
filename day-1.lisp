110714ce50dbe77dfbbc65bd189198dc2a93dc6c
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2024-12-01 21:22:38 +0100

    day-1

--- day-1.lisp
+++ day-1.lisp
@@ -4,3 +4,27 @@
   (:import-from :alexandria)
   (:import-from :serapeum))
 (in-package #:adventofcode-2024/day-1)
+
+(defun parse (file-name)
+  (loop :for l :in (uiop:read-file-lines file-name)
+        :for parsed = (util:parse-numbers-string l)
+        :collect (first parsed) :into list-1
+        :collect (second parsed) :into list-2
+        :finally (return
+                   (list list-1 list-2))))
+
+(defun part-1 (file-name)
+  (destructuring-bind (list-1 list-2) (parse file-name)
+    (loop :for x :in (sort list-1 #'<)
+          :for y :in (sort list-2 #'<)
+          :sum (abs (- x y)))))
+
+(defun part-2 (file-name)
+  (destructuring-bind (list-1 list-2) (parse file-name)
+    (loop :for x :in list-1
+          :sum (* x (count-if #'(lambda (y) (eql x y))
+                              list-2)))))
+
+(util:print-results 1
+                    (part-1 "./resources/input-day-1.1.txt")
+                    (part-2 "./resources/input-day-1.1.txt"))
--- util.lisp
+++ util.lisp
@@ -1,7 +1,13 @@
 (defpackage #:adventofcode-2024/util
+  (:nicknames #:util)
   (:use :cl)
   (:import-from :cl-ppcre)
-  (:import-from :alexandria))
+  (:import-from :alexandria)
+  (:export
+   :split-lines
+   :make-keyword
+   :parse-numbers-string
+   :print-results))
 (in-package #:adventofcode-2024/util)
 
 (defun split-lines (s)
@@ -13,3 +19,10 @@
 (defun parse-numbers-string (numbers-string)
   (loop :for m :in (ppcre:all-matches-as-strings "\\d+" numbers-string)
         :collect (parse-integer m)))
+
+(defun print-results* (day part-1 part-2)
+  (let ((sep "    "))
+    (format t "Day-~a~%~apart-1: ~a~%~apart-2: ~a~%" day sep part-1 sep part-2)))
+
+(defmacro print-results (day part-1 part-2)
+  `(time (print-results* ,day ,part-1 ,part-2)))
